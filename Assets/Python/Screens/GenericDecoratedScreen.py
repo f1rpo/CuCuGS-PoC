f3e3ae5226c3310009f654db508f6259eb7802d2
f1rpo <no.m@il.specified>
2021-05-18 21:32:38 +0200

    Remove trailing spaces from MNAI code

--- Assets/Python/Screens/GenericDecoratedScreen.py
+++ Assets/Python/Screens/GenericDecoratedScreen.py
@@ -11,13 +11,13 @@ ArtFileMgr = CyArtFileMgr()
 class GenericDecoratedScreen( object ) :
 
 	EXIT_ID = "Exit"
-	
+
 	def getScreen( self ) :
 		raise NotImplementedError( "Subclasses of GenericAdvisor must implement getScreen()" )
-	
+
 	def initDimensions( self ) :
 		screen = self.getScreen()
-		
+
 		if screen.getXResolution() > 1024 :
 			self.wScreen = max( 1024, screen.getXResolution() )
 			self.hScreen = max( 720, screen.getYResolution() )
@@ -26,35 +26,35 @@ class GenericDecoratedScreen( object ) :
 		self.xExitButton = self.wScreen - 30
 		# ccgs: Was -42. Want it to be closer to the center of the footer.
 		self.yExitButton = self.hScreen - 37
-		
+
 		return self.wScreen, self.hScreen # For convenience
-	
+
 	def addBackgroundHeaderFooter( self, szHeaderText ) :
 		wScreen, hScreen = self.wScreen, self.hScreen
 		screen = self.getScreen()
-		
+
 		self.hHeader = 55
 		self.hFooter = 55
 		self.yFooter = hScreen - self.hFooter
 		self.yMainArea = self.hHeader
 		self.hMainArea = self.hScreen - self.hHeader - self.hFooter
-		
+
 		# Background
 		screen.addDDSGFC("BackgroundPicture",
 				ArtFileMgr.getInterfaceArtInfo("SCREEN_BG_OPAQUE").getPath(),
 				0, 0, wScreen, hScreen, WidgetTypes.WIDGET_GENERAL, -1, -1 )
-		
+
 		# Header
 		screen.addPanel( "TopPanel", u"", u"", True, False, 0, 0, wScreen, self.hHeader,
 				PanelStyles.PANEL_STYLE_TOPBAR )
 		screen.setLabel( "TitleHeader", "Background", u"<font=4>" + szHeaderText + u"</font>",
 				CvUtil.FONT_CENTER_JUSTIFY, wScreen / 2, 8, 0, FontTypes.TITLE_FONT,
 				WidgetTypes.WIDGET_GENERAL, -1, -1 )
-		
+
 		# Footer
 		screen.addPanel( "BottomPanel", u"", u"", True, False, 0, self.yFooter,
 				wScreen, self.hFooter, PanelStyles.PANEL_STYLE_BOTTOMBAR )
-	
+
 	def addExitButton( self ) :
 		screen = self.getScreen()
 		szExitText = CyTranslator().getText("TXT_KEY_PEDIA_SCREEN_EXIT", ()).upper()
